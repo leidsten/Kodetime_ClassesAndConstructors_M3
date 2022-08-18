d84e3adb11e8dba1937fed34f129dd26d92c639e
leidsten <linn@getacademy.no>
2022-08-18 13:42:04 +0200

    x

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿
+namespace KodetimeM3_klasserOgConstructors
+{
+    public class Person
+    {
+        public string Name { get; private set; } //private set: kan bare endre data INNENFRA denne klassen
+        public int Age { get; private set; }
+        public int Height { get; private set; }
+        public int Weight { get; private set; }
+
+        private Random random = new Random();
+
+        public Person(string name, int age, int height, int weight)
+        {
+            Name = name;
+            Age = age;
+            Height = height;
+            Weight = weight;
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+            Age = 0;
+            Height = name == "Bjørbar" ? 75 : random.Next(30, 41);
+            Weight = random.Next(400, 7001);
+        }
+
+
+        public void PrintInfo()
+        {
+            string bonusPhrase = "You are a very average person..?";
+            //event om høyere enn så og så eller lavere enn så som så
+            if (Height >= 35) { bonusPhrase = "You are very tall!"; }
+            else if (Height <= 32) { bonusPhrase = "You a little bit short :)"; }
+            
+            Console.WriteLine($"Velkommen til verden {Name}! Du er {Age} år gammel, " +
+                $"{Height}cm høy, og veier {Weight}g! {bonusPhrase}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace KodetimeM3_klasserOgConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, M3!");
+            Console.WriteLine("Let's Write some Code!");
+
+            //random høyde & random vekt
+            //Random random = new Random();
+
+            var Bjørbar = new Person("Bjørbar");
+            var Adrian = new Person("Adrian");
+            var Marijani = new Person("Marijani");
+
+
+            Console.WriteLine("Who do you want to bring into this world?");
+
+            var userInputName = Console.ReadLine();
+
+            var userCreatedPerson = new Person(userInputName);
+
+            //var basket = new Person[]
+            //{
+            //    Bjørbar,
+            //    Adrian,
+            //    Marijani,
+            //    userCreatedPerson
+            //};
+
+            var basket = new List<Person>
+            {
+                    Bjørbar,
+                    Adrian,
+                    Marijani,
+                    userCreatedPerson
+            };
+
+            //for (var i = 0; i < basket.Length; i++)
+            //{
+            //    basket[i].PrintInfo();
+            //}
+
+            foreach (var person in basket)
+            {
+                person.PrintInfo();
+            }
+
+
+            //Bjørbar.PrintInfo();
+            //Adrian.PrintInfo();
+
+
+
+            //bruke en funksjon fra en annen klasse
+            //constructors!
+            //random!
+            //array / list
+
+
+            //person som blir født og har en random vekt og høyde
+
+        }
+    }
+}
